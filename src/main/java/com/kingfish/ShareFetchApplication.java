944da9ae84e1e3c939e17db4444c432949055df4
康真乐 <zhenle.kzl@alibaba-inc.com>
2016-07-30 21:44:03 +0800

    springboot project init

---
+++ src/main/java/com/kingfish/ShareFetchApplication.java
@@ -0,0 +1,12 @@
+package com.kingfish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareFetchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareFetchApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kingfish/ShareFetchApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kingfish;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShareFetchApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
